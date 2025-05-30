3f29ebe31a93541886177bc9d767def582aec480
Beatriz Oliveira <beatrizlimaoliveira1999@gmail.com>
2025-05-30 02:26:05 -0300

    Organizando repo e fazendo menu

--- Main.kt
+++ Main.kt
@@ -1,16 +1,4 @@
 
-fun main(){
-
-val alunos = mutableListOf<Aluno>()
-
-while (true){
-        val aluno = cadastrarAluno()
-        if (aluno.nome == "0") break
-        alunos.add(aluno)
-}
-
-println("\n--- Relatório de Alunos ---")
-for (aluno in alunos){
-    println(aluno.obterDetalhes())
-}
+fun main() {
+    AlunoUtils.mostrarMenu()
 }
